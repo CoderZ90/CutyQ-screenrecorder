7e263115054849f9a466d45c8d0704e1a38ae547
DarkCoder100 <singhsima725@gmail.com>
2021-10-12 13:20:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    "*.html"
+  ],  
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"]
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
